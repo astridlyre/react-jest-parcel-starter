10b95d1c0d0cf4f9fdc528cd3d74b97c96aa1a28
Erin Burton <eb@flat9.dev>
2021-12-22 01:56:52 -0800

    added favicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import * as styles from './App.module.css'
 
+const imageUrl = new URL('cage.jpg', import.meta.url)
+
 export default function App() {
   return (
     <main className={styles.root}>
-      <h1>Hello World</h1>
+      <h1 className={styles.header}>Hello World</h1>
+      <figure className={styles.figure}>
+        <img className={styles.img} src={imageUrl} alt='Nicholas Cage' />
+        <figcaption className={styles.caption}>
+          I'm going to steal the Declaration of Independence.
+        </figcaption>
+      </figure>
     </main>
   )
 }
