1b22513b77989c92f9381bf102e162cc89281379
Erin Burton <eb@flat9.dev>
2021-12-24 02:56:19 -0800

    changed image import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import * as styles from './App.module.css'
-
-const imageUrl = new URL('cage.jpg', import.meta.url)
+import image from './cage.jpg'
 
 export default function App() {
   return (
     <main className={styles.root}>
       <h1 className={styles.header}>Hello World</h1>
       <figure className={styles.figure}>
-        <img className={styles.img} src={imageUrl} alt='Nicholas Cage' />
+        <img className={styles.img} src={image} alt='Nicholas Cage' />
         <figcaption className={styles.caption}>
           I'm going to steal the Declaration of Independence.
         </figcaption>
