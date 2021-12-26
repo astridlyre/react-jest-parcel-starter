ea75399d31c7df6387a15efb8b23d11a10777844
Erin Burton <eb@flat9.dev>
2021-12-26 04:47:16 -0800

    added prettierrc and eslint rules

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <main className={styles.root}>
       <h1 className={styles.header}>Hello World</h1>
       <figure className={styles.figure}>
-        <img className={styles.img} src={image} alt='Nicholas Cage' />
+        <img className={styles.img} src={image} alt="Nicholas Cage" />
         <figcaption className={styles.caption}>
           I&apos;m going to steal the Declaration of Independence.
         </figcaption>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import App from './App.js'
 
-render(<App />, document.getElementById('root'))
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+)
