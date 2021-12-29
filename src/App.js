5ff3fb877ce46366e562e0ecf096a345eb35fa1d
Erin Burton <eb@flat9.dev>
2021-12-29 05:59:34 -0800

    updated imports to avoid defaults

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import * as styles from './App.module.css'
 import image from './cage.jpg'
 
-export default function App() {
+export function App() {
   return (
     <main className={styles.root}>
       <h1 className={styles.header}>Hello World</h1>
       <figure className={styles.figure}>
-        <img className={styles.img} src={image} alt="Nicholas Cage" />
+        <img className={styles.img} src={image} alt='Nicholas Cage' />
         <figcaption className={styles.caption}>
           I&apos;m going to steal the Declaration of Independence.
         </figcaption>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
-import App from './App.js'
+import { App } from './App.js'
 
 render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
